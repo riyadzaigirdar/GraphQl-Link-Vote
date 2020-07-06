a89e01da029cd5d9b9fb335395e3247d603bf10f
riyadzaigirdar <riyadzaigir280@gmail.com>
2020-07-06 10:03:49 +0600

    new first commit

--- hackernews/urls.py
+++ hackernews/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
+    path('', include('links.urls')),
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
